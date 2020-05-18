51279e6337297cf1f0199db016adf8834db51f0d
Juan David Neisa Paramo <jdneisap@correo.udistrital.edu.co>
2020-05-17 22:29:16 -0500

    Primer componente agregado -Mapa-. Bootstrap agregado y Jquery.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapaComponent } from './mapa/mapa.component';
+
+const router: Routes = [
+  {
+    path: 'mapa',
+    component: MapaComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(router)
   ],
   providers: [],
   bootstrap: [AppComponent]
